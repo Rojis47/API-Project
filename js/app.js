7444d1b6fc8a8ac13dbd62800f8a2f77f706c5cd
Rogelio Servin <manchaster1509@gmail.com>
2023-04-06 20:26:16 -0500

    complete

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+//* Utility function to set an element as active
 const setActive = (elm, selector) => {
   const active = "active";
   if (document.querySelector(`${selector}.${active}`) !== null) {
@@ -7,38 +7,74 @@ const setActive = (elm, selector) => {
   elm.classList.add(active);
 };
 
+//* Filter cards based on active type and display filters
 const filterCards = () => {
-  const cardItemLocation = document.querySelectorAll('[data-location]');
-  const cardItemType = document.querySelectorAll('[data-item]');
+  const cardItemLocation = document.querySelectorAll("[data-location]");
 
-  const activeTypeFilter = document.querySelector('.filter-link.active').dataset.filter;
-  const activeDisplayFilter = document.querySelector('.filter-location.active').dataset.display;
+  const activeTypeFilter = document.querySelector(".filter-link.active").dataset
+    .filter;
+  const activeDisplayFilter = document.querySelector(".filter-location.active")
+    .dataset.display;
 
   cardItemLocation.forEach((card) => {
     const cardType = card.dataset.item;
     const cardLocation = card.dataset.location;
 
-    if ((activeTypeFilter === 'all' || cardType === activeTypeFilter) && cardLocation === activeDisplayFilter) {
-      card.style.display = 'block';
+    if (
+      (activeTypeFilter === "all" || cardType === activeTypeFilter) &&
+      cardLocation === activeDisplayFilter
+    ) {
+      card.style.display = "block";
     } else {
-      card.style.display = 'none';
+      card.style.display = "none";
     }
   });
 };
 
-const initializeFilters = () => {
-  const cardItemType = document.querySelectorAll('[data-item]');
-  const filterByType = document.querySelectorAll("[data-filter]");
-  filterCardsType(filterByType, cardItemType);
+//* filter a-z
+const sortCards = (order) => {
+  const cardsContainer = document.getElementById("poke-cards");
+  const cards = Array.from(cardsContainer.children);
 
-  const cardItemLocation = document.querySelectorAll('[data-location]');
-  const filterByLocation = document.querySelectorAll('[data-display]');
-  filterCardsDisplay(filterByLocation, cardItemLocation);
+  cards.sort((a, b) => {
+    const nameA = a.querySelector(".poke-name").textContent.toLowerCase();
+    const nameB = b.querySelector(".poke-name").textContent.toLowerCase();
 
-  document.querySelector('.filter-location.active').click();
+    if (order === "az") {
+      return nameA.localeCompare(nameB);
+    } else {
+      return nameB.localeCompare(nameA);
+    }
+  });
+
+  cards.forEach((card) => {
+    cardsContainer.appendChild(card);
+  });
+
+  filterCards();
 };
 
-const filterCardsType = (filterByType) => {
+document.getElementById("sort-az").addEventListener("click", () => {
+  sortCards("az");
+});
+
+document.getElementById("sort-za").addEventListener("click", () => {
+  sortCards("za");
+});
+
+//* Initialize filter event listeners
+const initializeFilters = () => {
+  const filterByType = document.querySelectorAll("[data-filter]");
+  const filterByLocation = document.querySelectorAll("[data-display]");
+
+  addFilterTypeEventListeners(filterByType);
+  addFilterDisplayEventListeners(filterByLocation);
+
+  document.querySelector(".filter-location.active").click();
+};
+
+//* Add event listeners for type filters
+const addFilterTypeEventListeners = (filterByType) => {
   for (const link of filterByType) {
     link.addEventListener("click", function () {
       setActive(link, ".filter-link");
@@ -47,8 +83,8 @@ const filterCardsType = (filterByType) => {
   }
 };
 
-
-const filterCardsDisplay = (filterByLocation) => {
+//* Add event listeners for display filters
+const addFilterDisplayEventListeners = (filterByLocation) => {
   for (const link of filterByLocation) {
     link.addEventListener("click", function () {
       setActive(link, ".filter-location");
@@ -57,23 +93,66 @@ const filterCardsDisplay = (filterByLocation) => {
   }
 };
 
+//* Create filter buttons with type count
+const createFilterButtons = (pokemons) => {
+  const typeCounts = getTypeCounts(pokemons);
+  renderFilterButtons(typeCounts);
+};
+
+//* Count the number of each type in the provided pokemons
+const getTypeCounts = (pokemons) => {
+  const typeCounts = {};
+
+  pokemons.forEach((pokemon) => {
+    const mainType = pokemon.types[0].type.name;
+    if (!typeCounts[mainType]) {
+      typeCounts[mainType] = 0;
+    }
+    typeCounts[mainType]++;
+  });
+
+  return typeCounts;
+};
+
+//* Render filter buttons with type counts
+const renderFilterButtons = (typeCounts) => {
+  const filterList = document.getElementById("filter-btn");
+  filterList.innerHTML =
+    '<li data-filter="all" class="filter-btn filter-link type-all active">All</li>';
+
+  Object.keys(typeCounts).forEach((type) => {
+    const button = document.createElement("li");
+    button.setAttribute("data-filter", type);
+    button.classList.add("filter-btn", "filter-link", `type-${type}`);
+    button.textContent = `${type.charAt(0).toUpperCase() + type.slice(1)} ${
+      typeCounts[type]
+    }`;
+    filterList.appendChild(button);
+  });
+
+  addFilterTypeEventListeners(document.querySelectorAll("[data-filter]"));
+};
+
 const cardHtml = (pokemon) => {
   const hpStat = pokemon.stats.find((stat) => stat.stat.name === "hp");
   const card = `
     <div class="card" data-item="${pokemon.types[0].type.name}" data-location="collection">
-      <div class="card-header">
+    <div class="card-overlay"></div>
+    <div class="card-header">
         <h3 class="poke-name">${pokemon.name}</h3>
         <span class="poke-hp">HP ${hpStat ? hpStat.base_stat : "N/A"}</span>
-      </div>
-      <div class="poke-img">
-        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-      </div>
-      <div class="poke-abilities">
-        <h4 class="ability-title">Ability: ${pokemon.abilities[0].ability.name}</h4>
-        <button class="favorite-btn" data-status="collections">Add to Favorites</button>
-      </div>
     </div>
-  `;
+    <div class="poke-img">
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+    </div>
+    <div class="poke-abilities">
+        <h4 class="ability-title">Ability: ${
+          pokemon.abilities[0].ability.name
+        }</h4>
+        <button class="favorite-btn" data-status="collections">Add to Favorites</button>
+    </div>
+    </div>
+`;
   return card;
 };
 
@@ -83,45 +162,57 @@ const addOrRemoveFromFavorites = (card) => {
     const status = favoriteButton.getAttribute("data-status");
 
     if (status === "collections") {
-      document.getElementById("poke-cards").removeChild(card);
-      document.getElementById("favorite-cards").appendChild(card);
-      favoriteButton.textContent = "Remove from Favorites";
-      favoriteButton.setAttribute("data-status", "favorites");
-      card.setAttribute("data-location", "favorite");
+      moveCard(card, "poke-cards", "favorite-cards", "Remove from Favorites", "favorites", "favorite");
     } else {
-      document.getElementById("favorite-cards").removeChild(card);
-      document.getElementById("poke-cards").appendChild(card);
-      favoriteButton.textContent = "Add to Favorites";
-      favoriteButton.setAttribute("data-status", "collections");
-      card.setAttribute("data-location", "collection");
+      moveCard(card, "favorite-cards", "poke-cards", "Add to Favorites", "collections", "collection");
     }
 
-    initializeFilters()
+    initializeFilters();
   });
 };
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${20}`;
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    const fetchDataPromises = data.results.map((result) => {
-      const pokemonUrl = result.url;
-      return fetch(pokemonUrl)
-      .then((res) => res.json())
-        .then((pokemonData) => {
-          const wrapper = document.createElement("div");
-          wrapper.innerHTML = cardHtml(pokemonData).trim();
-          const cardElement = wrapper.firstChild;
-          document.getElementById("poke-cards").appendChild(cardElement);
-          addOrRemoveFromFavorites(cardElement);
-        })
-        .catch((err) => console.error("error:" + err));
-    });
-    Promise.all(fetchDataPromises).then(() => {
-      initializeFilters();
-    });
-  })
-  .catch((err) => console.error("error:" + err));
-  
+const moveCard = (
+  card,
+  fromId,
+  toId,
+  buttonText,
+  buttonStatus,
+  cardLocation
+) => {
+  document.getElementById(fromId).removeChild(card);
+  document.getElementById(toId).appendChild(card);
+  card.querySelector(".favorite-btn").textContent = buttonText;
+  card.querySelector(".favorite-btn").setAttribute("data-status", buttonStatus);
+  card.setAttribute("data-location", cardLocation);
+};
+const fetchPokemons = (url, limit) => {
+  return fetch(`${url}?offset=0&limit=${limit}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const fetchDataPromises = data.results.map((result) => {
+        return fetch(result.url)
+          .then((res) => res.json())
+          .catch((err) => console.error("error:" + err));
+      });
+      return Promise.all(fetchDataPromises);
+    })
+    .catch((err) => console.error("error:" + err));
+};
 
+const renderCards = (pokemons) => {
+  pokemons.forEach((pokemonData) => {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = cardHtml(pokemonData).trim();
+    const cardElement = wrapper.firstChild;
+    document.getElementById("poke-cards").appendChild(cardElement);
+    addOrRemoveFromFavorites(cardElement);
+  });
+};
 
+const url = "https://pokeapi.co/api/v2/pokemon";
+const limit = 20;
+fetchPokemons(url, limit).then((pokemons) => {
+  renderCards(pokemons);
+  createFilterButtons(pokemons);
+  initializeFilters();
+});
